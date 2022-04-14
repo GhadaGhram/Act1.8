f4cfd0345eae097d8b76112bee7d739f172252ee
GhadaGhram <103033611+GhadaGhram@users.noreply.github.com>
2022-04-14 22:39:43 +0200

    Update MusiqueImpl.java

--- MusiqueImpl.java
+++ MusiqueImpl.java
@@ -1,4 +1,4 @@
-package Streaming.copy;
+package Streamingdemusique;
 
 public class MusiqueImpl implements Musique{
 	private String titre;
@@ -6,46 +6,69 @@ public class MusiqueImpl implements Musique{
 	private String genre;
 	private String interprete;
 	
-	public String getTitre() {
-		return titre;
-	}
-
-	public String getAuteur() {
-		return auteur;
-	}
-
-	public String getGenre() {
-		return genre;
-	}
-
-	
-		public MusiqueImpl(String titre, String auteur, String genre, String interprete) {
+		
+public MusiqueImpl(String titre, String auteur, String genre, String interprete) {
 		this.titre = titre;
 		this.auteur = auteur;
 		this.genre = genre;
 		this.interprete = interprete;
 	}
 
+	public String getTitre() {
+	return titre;
+}
+
+public void setTitre(String titre) {
+	this.titre = titre;
+}
+
+public String getAuteur() {
+	return auteur;
+}
+
+public void setAuteur(String auteur) {
+	this.auteur = auteur;
+}
+
+public String getGenre() {
+	return genre;
+}
+
+public void setGenre(String genre) {
+	this.genre = genre;
+}
+
+public String getInterprete() {
+	return interprete;
+}
+
+
+public void setInterprete(String interprete) {
+	this.interprete = interprete;
+}
 
 	@Override
-		public String toString() {
-			return "MusiqueImpl [titre=" + titre + ", auteur=" + auteur + ", genre=" + genre + ", interprete="
+public String toString() {
+	return "MusiqueImpl [titre=" + titre + ", auteur=" + auteur + ", genre=" + genre + ", interprete="
 					+ interprete + "]";
 		}
 
 	@Override
-	public void afficher() {
-			System.out.println(toString());
+public void afficher() {
+		System.out.println(toString());
 	}
-	@Override
-	public void rechercherAuteur(MusiqueImpl music1,String auteur) {
-		if(music1.getTitre().equals(auteur)){
-		    System.out.println("Existe ");}
-        else{
-		    
-		    System.out.println("N'existe pas");}
-						
 	
-		}
+	@Override
+	public void verifEgale(Streaming.MusiqueImpl music1, Streaming.MusiqueImpl music2) {
+		if((music1.getTitre()).equals(music2.getTitre())
+				& (music1.getAuteur()).equals(music2.getAuteur())
+				& (music1.getGenre()).equals(music2.getGenre()))
+					{
+				    System.out.println("les deux morceaux sont identiques");}
+	            else{
+				    
+				    System.out.println("les deux morceaux ne sont pas identiques");}
+		
+			}
 
-	}
+		}
