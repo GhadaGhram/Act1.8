5ab46682d50b8813843fb0109f21d1e4354631fe
GhadaGhram <103033611+GhadaGhram@users.noreply.github.com>
2022-04-15 03:52:03 +0200

    Update PlayListeUI.java

--- PlayListeUI.java
+++ PlayListeUI.java
@@ -1,12 +1,72 @@
 package Streamingdemusique;
-import java.io.ObjectInputStream.GetField;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
 public class PlayListeUI {
 
-	public static void main(String[] args) {
+	//Lire depuis un fichier
+	public static ArrayList<MusiqueImpl> storePlaylistFromFile(ArrayList<MusiqueImpl> playliste){
+		System.out.println("Lire depuis un fichier.txt");
+		File fileL=new File("C:/Users/Ce-Pc/eclipse-workspace/Ghada/src/Streamingdemusique//music.txt");
 		
+		if(!fileL.exists())
+		{try {
+		    fileL.createNewFile();}
+		  catch (Exception e) {e.printStackTrace();}
+		}
+		else
+	{
+		try{
+		FileReader fileReader = new FileReader(fileL);
+		BufferedReader reader=new BufferedReader(fileReader);
+		String line=reader.readLine();
+		while(line!=null){
+		System.out.println(line);
+		line=reader.readLine();
+		String[] music =line.split(",");
+	    playliste.add(new MusiqueImpl(music[0],music[1], music[2], music[3]));
+		}
+		reader.close();
+		}catch(IOException e){
+		e.printStackTrace();
+		}
+	}
+		return playliste;  	
+		
+	}
+			
+	 //Enregitrer dans un autre fichier
+		public static void storePlaylistToFile(ArrayList<MusiqueImpl> playliste) throws IOException  {	
+		System.out.println("Enregistrer la playliste dans FileE");
+		
+		File fileE=new File ("C://Users//Ce-Pc//Desktop//Talan-Academy/Enreg.txt");
+		if(!fileE.exists())
+		{try {
+		    fileE.createNewFile();}
+		    catch (Exception e) {e.printStackTrace();}}
+		else {		 
+		FileWriter fileWriter = new FileWriter(fileE);
+		BufferedWriter writer = new BufferedWriter (fileWriter);			 		      
+		      
+		        for(MusiqueImpl music : playliste) {
+		    	  
+		    	  writer.write(music.getTitre() + ",");
+		    	  writer.write(music.getAuteur() + ",");
+		    	  writer.write(music.getGenre() + ",");
+		    	  writer.write(music.getInterprete());
+		    	  writer.newLine();
+		                                            }
+		      writer.close();
+		      }
+		     		     }		
+		//----------------------------------------
+		public static void main(String[] args) throws IOException {	
 		MusiqueImpl music1=new MusiqueImpl("Hero", "Celine", "romance", "interprete1");
 		MusiqueImpl music2=new MusiqueImpl("Je veux", "Zaz","amour", "interprete2");
 		
@@ -19,9 +79,11 @@ public class PlayListeUI {
 		
 		ArrayList<MusiqueImpl> playliste1 = new ArrayList<>();
 		ArrayList<MusiqueImpl> playliste2 = new ArrayList<>() ;
+		ArrayList<MusiqueImpl> playliste3 = new ArrayList<>() ;
 		
 		PlayListeImpl pl1=new PlayListeImpl("Popular music", 3, playliste1);
 		PlayListeImpl pl2 =new PlayListeImpl("Rock style", 2, playliste2);
+		
 		
 		
 //Ajouter une musique à une palyliste:
@@ -80,5 +142,11 @@ public class PlayListeUI {
 					for (PlayListeImpl playlisteImpl1 : streaming) {
 					System.out.println(playlisteImpl1.getNomMusique() + " ");
 				}
+					
+					storePlaylistFromFile(playliste3);
+				    storePlaylistToFile(playliste3);
+				 	
+					System.out.println(streaming.toString());
 				
 }}
+
